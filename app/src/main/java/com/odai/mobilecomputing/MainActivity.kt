3843d4df611d9ab58749fe92b761cd46bddd0b8a
Aodai <Aodai@users.noreply.github.com>
2022-03-17 15:31:50 +0200

    Reading accelerometer data.

--- app/src/main/java/com/odai/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/odai/mobilecomputing/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.odai.mobilecomputing
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private lateinit var tvSensorValue: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvSensorValue = findViewById(R.id.tvSensorValue)
+        sensorInit()
+    }
+
+    private fun sensorInit() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST, SensorManager.SENSOR_DELAY_FASTEST)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val sides = event.values[0]
+            val upDown = event.values[1]
+            if(upDown.toInt() == 0 && sides.toInt() == 0)
+                tvSensorValue.text = "Phone is flat"
+                //tvSensorValue.setText("Phone is flat")
+            else
+                tvSensorValue.text = "Phone is tilted!"
+                //tvSensorValue.setText("Phone is tilted!")
+        }
+    }
+
+    override fun onAccuracyChanged(event: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 }
