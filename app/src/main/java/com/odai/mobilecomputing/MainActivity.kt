46ad3823f623e49c7946e053b535a94991586d66
Aodai <Aodai@users.noreply.github.com>
2022-03-23 00:36:11 +0200

    Added some fragments and setup navigation.

--- app/src/main/java/com/odai/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/odai/mobilecomputing/MainActivity.kt
@@ -12,18 +12,18 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-    private lateinit var sensorManager: SensorManager
-    private lateinit var tvSensorValue: TextView
-    private lateinit var toggle : ActionBarDrawerToggle
+class MainActivity : AppCompatActivity() {
+    lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
@@ -32,16 +32,26 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
             when(it.itemId) {
-                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
-                R.id.nav_map -> Toast.makeText(applicationContext, "Clicked Map", Toast.LENGTH_SHORT).show()
-                R.id.nav_camera -> Toast.makeText(applicationContext, "Clicked Camera", Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> replaceFragmenmt(HomeFragment(), it.title.toString())
+                R.id.nav_map -> replaceFragmenmt(MapFragment(), it.title.toString())
+                R.id.nav_camera -> replaceFragmenmt(CameraFragment(), it.title.toString())
             }
             true
         }
+    }
 
-        tvSensorValue = findViewById(R.id.tvSensorValue)
-        sensorInit()
+    private fun replaceFragmenmt(fragment : Fragment, title : String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -49,32 +59,5 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             return true
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    private fun sensorInit() {
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
-            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST, SensorManager.SENSOR_DELAY_FASTEST)
-        }
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-            val sides = event.values[0]
-            val upDown = event.values[1]
-            if(upDown.toInt() == 0 && sides.toInt() == 0)
-                tvSensorValue.text = getString(R.string.flat)
-            else
-                tvSensorValue.text = getString(R.string.tilted)
-        }
-    }
-
-    override fun onAccuracyChanged(event: Sensor?, accuracy: Int) {
-        return
-    }
-
-    override fun onDestroy() {
-        sensorManager.unregisterListener(this)
-        super.onDestroy()
     }
 }
